0127e2f71be0c96fbd4b199fd5f77ecdf8ffe8f6
wesleyabreeuu <wesleyavivabr@gmail.com>
2025-02-04 20:02:46 -0300

    previsao do tempo

--- previsao_tempo.php
+++ previsao_tempo.php
@@ -4,5 +4,9 @@ $temperatura = 30;
 
 if ($temperatura >= 30)
 {
-    echo 'A temperatura do dia de amanhã será de '. $temperatura . ''
+    echo 'A temperatura do dia de amanhã será de '. $temperatura . 'º. Amanhã será um dia ensolarado.';
+}
+else
+{
+    echo 'A temperatura do dia de amanhã será de '. $temperatura . 'º. Amanhã será um dia nublado.';
 }
